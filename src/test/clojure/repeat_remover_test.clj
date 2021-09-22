c4a4cca3443b03f990552838f4511db36f95e0f3
aardvark <aardvark@bk.ru>
2021-09-22 15:01:44 +0300

    move from clojure test to junit test

--- src/test/clojure/repeat_remover_test.clj
+++ src/test/clojure/repeat_remover_test.clj
@@ -1,9 +1,21 @@
 (ns repeat-remover-test
     (:require [clojure.test :refer [deftest is]])
-    (:import (org.example.testing RepeatRemover)))
+  (:import (org.example.testing RepeatRemover)
+           (org.junit.jupiter.api Assertions))
+    (:gen-class
+      :name org.example.testing.RepeatRemoverClojureTest
+      :main false
+      :methods [[^{org.junit.jupiter.api.Test {}}
+                 testStaticCall [] void]
+                [^{org.junit.jupiter.api.Test {}}
+                 testInstanceCall [] void]]
+      :prefix "")
+  )
 
-(deftest repeat-remover-static-test
-         (is (= "abc" (RepeatRemover/staticRemove "aabcc"))))
+(def subj (RepeatRemover.))
 
-(deftest repeat-remover-instance-test
-  (is (= "abc" (.remove (RepeatRemover.) "aabbcc"))))
+(defn testStaticCall [this]
+  (Assertions/assertEquals "ABC" (RepeatRemover/staticRemove "AABCC")))
+
+(defn testInstanceCall [this]
+  (Assertions/assertEquals "ABC" (.remove subj "AABCC")))
